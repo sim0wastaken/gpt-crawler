1f3670b65f13921923a32a339bb011cc450f9668
sim0wastaken <104425606+sim0wastaken@users.noreply.github.com>
2023-12-05 13:56:56 +0100

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
+  url: "https://www.unito.it/avvisi/progetto-compiticasa-curare-la-fragilita-educativa-bando-collaborazioni-part-time-studenti-1",
+  match: "https://www.unito.it/avvisi/**",
   maxPagesToCrawl: 50,
   outputFileName: "output.json",
 };
